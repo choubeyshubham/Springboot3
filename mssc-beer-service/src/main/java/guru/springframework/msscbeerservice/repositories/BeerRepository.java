3301d047f18df392724fc6a2899441e838238982
choubeyshubham <shubhamchoubey96@gmail.com>
2025-03-04 16:30:24 +0530

    15 commit

--- mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -12,7 +12,6 @@ import java.util.UUID;
  * Created by jt on 2019-05-17.
  */
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
-
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
     Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
